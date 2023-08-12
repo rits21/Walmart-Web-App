434a1088a20eefe4b4eab0543c2bee4a56c63cf7
Rittika Majumder <119842177+rits21@users.noreply.github.com>
2023-08-12 14:02:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const productList = document.querySelector('.product-list');
-    const searchInput = document.getElementById('searchInput');
-    const searchButton = document.getElementById('searchButton');
+document.addEventListener('DOMContentLoaded', function() {
+    let products = document.querySelector('.products');
+    async function fetchProducts(url) {
+        try {
+            let data = await fetch(url);
+            let response = await data.json();
 
-    const products = [
-        { id: 1, name: "Women's Floral Dress", price: "$49.99", image: "product1.jpg" },
-        { id: 2, name: "Men's Casual Shirt", price: "$29.99", image: "product2.jpg" },
-        { id: 3, name: "Teenage Girls Wear", price: "$54.99", image: "product3.jpg" },
-        { id: 4, name: "Teenage Boys Weart", price: "$39.99", image: "product4.jpg" }
-        // Add more products as needed
-    ];
-
-    // Display initial product list
-    displayProducts(products);
-
-    // Add event listener to the search button
-    searchButton.addEventListener("click", handleSearch);
-
-    function handleSearch() {
-        const searchTerm = searchInput.value.toLowerCase();
-        const filteredProducts = products.filter(product =>
-            product.name.toLowerCase().includes(searchTerm)
-        );
-
-        displayProducts(filteredProducts);
+            for (let i = 0; i < response.length; i++) {
+                let description = response[i].description;
+                let title = response[i].title;
+                products.innerHTML += `
+            <div class="product">
+                <img src="${response[i].images[0]}" alt="${
+                response[i].category.name
+                }" class="product-img">
+                <div class="product-content">
+                    <h2 class="product-title">${
+                        title.length > 18 ? title.substring(0, 18).concat(' ...') : title
+                    }</h2>
+                    <p class="product-description">${
+                        description.length > 80
+                        ? description.substring(0, 80).concat(' ...more')
+                        : description
+                    }</p>
+                    <div class="product-price-container">
+                        <h3 class="product-price">$${response[i].price}</h3>
+                        <a href="#!" data-productId="${
+                            response[i].id
+                        }" class="add-to-cart"><ion-icon name="cart-outline"></ion-icon></a>
+                    </div>
+                </div>
+            </div>`;
+            }
+        } catch (err) {
+            console.log(err);
+        }
     }
-
-    function displayProducts(products) {
-        productList.innerHTML = "";
-
-        products.forEach(product => {
-            const productCard = document.createElement('div');
-            productCard.classList.add('product-card');
-            productCard.dataset.productId = product.id;
-            productCard.innerHTML = `
-                <img src="${product.image}" alt="${product.name}">
-                <h3>${product.name}</h3>
-                <p>${product.price}</p>
-            `;
-            productList.appendChild(productCard);
-        });
-    }
+    fetchProducts('https://api.escuelajs.co/api/v1/products');
 });
+
